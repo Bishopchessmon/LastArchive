534cb37326a2c3051bf8456318d902581f37bca2
Bishopchessmon <dominic.romero000@gmail.com>
2020-10-03 17:59:20 +0800

    generate base page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModuleModule } from './module/material-module/material-module.module';
+import { NavbarComponent } from './page/common/navbar/navbar.component';
+import { FooterComponent } from './page/common/footer/footer.component';
+import { HomepageComponent } from './page/homepage/homepage.component';
+import { AboutComponent } from './page/about/about.component';
+import { ProjectsComponent } from './page/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomepageComponent,
+    AboutComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
