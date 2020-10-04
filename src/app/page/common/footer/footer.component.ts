d8f0b4dd2185f99ab939aa259eeeee3d2bd010e0
Bishopchessmon <dominic.romero000@gmail.com>
2020-10-05 00:14:40 +0800

    Add Footer

--- src/app/page/common/footer/footer.component.ts
+++ src/app/page/common/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  sb_horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  sb_verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
-  constructor() { }
+  constructor(private _snackBar: MatSnackBar) {}
+
 
   ngOnInit(): void {
   }
 
+  snackbarEmail() {
+    this._snackBar.open('Email: *******.romero***@****.com', 'Close', {
+      duration: 5000,
+      horizontalPosition: this.sb_horizontalPosition,
+      verticalPosition: this.sb_verticalPosition,
+    });
+  }
+
+  snackbarHomeWork() {
+    this._snackBar.open('Discord: Niks#9049', 'Close', {
+      duration: 5000,
+      horizontalPosition: this.sb_horizontalPosition,
+      verticalPosition: this.sb_verticalPosition,
+    });
+  }
 }
