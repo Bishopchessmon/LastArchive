8a0f7400b697eea875a2480acce94101a08bed60
Bishopchessmon <dominic.romero000@gmail.com>
2020-10-04 19:27:21 +0800

    create a not found page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './page/common/not-found/not-found.component';
+import { HomepageComponent } from './page/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomepageComponent
+}, {
+  path: 'home',
+  redirectTo: '/',
+  pathMatch: 'full',
+}, { 
+  path: 'not-found', 
+  component: NotFoundComponent
+}, { 
+  path: '**', 
+  component: NotFoundComponent
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
